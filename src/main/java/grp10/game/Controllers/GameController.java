6288c490f41607352f89fbab70a2344720ac3a90
Nnn2 <nnn2@hi.is>
2021-10-27 19:01:53 +0000

    home basic setup

--- src/main/java/grp10/game/Controllers/GameController.java
+++ src/main/java/grp10/game/Controllers/GameController.java
@@ -1,2 +1,16 @@
-package grp10.game.Controllers;public class GameController {
+package grp10.game.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class GameController {
+
+    @RequestMapping("/")
+    public String GameController(){
+        //business logic
+        // call method in service class
+        //add some data to model
+        return"home";
+    }
 }
