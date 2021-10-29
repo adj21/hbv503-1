8333cf1bc8166e9207042f380909b292dd3eebc4
Nnn2 <nnn2@hi.is>
2021-10-29 15:35:09 +0000

    Update WordController.java

--- src/main/java/grp10/game/Controllers/WordController.java
+++ src/main/java/grp10/game/Controllers/WordController.java
@@ -1,3 +1,16 @@
 package grp10.game.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class WordController {
+
+    @RequestMapping("/")
+    public String WordController(){
+        //business logic
+        // call method in service class
+        //add some data to model
+        return"";
+    }
 }
